618fb1e13acd8206ae3088dfb0ec839bba776a10
harshithap99 <46523187+harshithap99@users.noreply.github.com>
2020-09-04 13:48:29 +0530

    For understanding

---
+++ app/src/main/java/com/example/amrita_placements/registration.java
@@ -0,0 +1,22 @@
+package com.example.amrita_placements;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.SpannedString;
+import android.text.TextPaint;
+import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.Collator;
+
+public class registration extends AppCompatActivity {
+
+    }
