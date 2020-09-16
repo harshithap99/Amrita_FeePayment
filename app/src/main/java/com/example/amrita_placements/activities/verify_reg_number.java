b5f03be1f7e8aa4107161d0d2ee9989bc6aab11e
930r934 <60056514+930r934@users.noreply.github.com>
2020-09-16 13:59:45 +0530

    homepage1.xml is the homepage

--- app/src/main/java/com/example/amrita_placements/activities/verify_reg_number.java
+++ app/src/main/java/com/example/amrita_placements/activities/verify_reg_number.java
@@ -15,7 +15,7 @@ public class verify_reg_number extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.homepage);
+        setContentView(R.layout.homepage1);
 
     }
 
