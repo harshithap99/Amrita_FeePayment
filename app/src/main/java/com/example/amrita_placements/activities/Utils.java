4f54eb1fa0343658b8e929d36c779995965311ab
930r934 <60056514+930r934@users.noreply.github.com>
2020-11-06 22:38:31 +0530

    mail pass oori

--- app/src/main/java/com/example/amrita_placements/activities/Utils.java
+++ app/src/main/java/com/example/amrita_placements/activities/Utils.java
@@ -3,6 +3,6 @@ package com.example.amrita_placements.activities;
 public class Utils {
 
     public static final String EMAIL = "feepayossd2020@gmail.com";
-    public static final String PASSWORD = "V@KKaPan94810";
+    public static final String PASSWORD = "";
 
 }
